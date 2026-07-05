3720933d25c8f7cbced011d9cb31fce762a87e40
jdjdjdjdndn888 <294801295+jdjdjdjdndn888@users.noreply.github.com>
2026-07-05 10:05:58 +0000

    Add custom coinflip icon, make dice icon bigger in sidenav

--- components/sidenav/Sidenav.jsx
+++ components/sidenav/Sidenav.jsx
@@ -17,8 +17,8 @@ import Logo from "@/components/logo/Logo";
 
 const navItems = [
   { label: "Home", icon: HomeIcon, href: "/" },
-  { label: "Coinflip", icon: () => <Coins className="w-5 h-5" />, href: "/coinflip" },
-  { label: "Dice", icon: () => <img src="/dice-icon.png" alt="Dice" className="w-7 h-7 object-contain" />, href: "/dice" },
+  { label: "Coinflip", icon: () => <img src="/coinflip-icon.png" alt="Coinflip" className="w-8 h-8 object-contain" />, href: "/coinflip" },
+  { label: "Dice", icon: () => <img src="/dice-icon.png" alt="Dice" className="w-8 h-8 object-contain" />, href: "/dice" },
   { label: "Jackpot", icon: () => <Dices className="w-5 h-5" />, href: "/jackpot" },
   { label: "Upgrader", icon: () => <TrendingUp className="w-5 h-5" />, href: "/upgrader" },
   { label: "Trades", icon: MarketPlaceIcon, href: "/trades" },
